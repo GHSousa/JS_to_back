f34222bfd3d5383f3f98c03c6059f1e6280bab99
Gabriel Hiago de Sousa <gabrielhiago96@hotmail.com>
2022-10-13 01:27:17 -0300

    Atualização aula 05 JS_to_back 01/02

---
+++ JS_to_back05.js
@@ -0,0 +1,40 @@
+//funcoes
+
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// TRES FORMAS DE ESCREVER FUNÇÕES
+// FORMA 01 - DECLARAÇÃO DE FUNÇÃO
+
+//01 declara a função
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+imprimeTexto(soma());
+//02 executa a função (quando c chama ela)
+
+imprimeTexto("isso por que eu só tava testando voce");
+
+function soma (){
+    return 2 + 2; //return tem que ser a ultima linha da função.
+}
+
+//console.log(soma());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
