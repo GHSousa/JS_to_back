640174a07c3f8b482a4bd158127ffecdedd81db4
Gabriel Hiago de Sousa <gabrielhiago96@hotmail.com>
2022-10-13 00:32:11 -0300

    Aula 04 JS_to_back

---
+++ JS_to_back04.js
@@ -0,0 +1,39 @@
+//COMPARACOES 
+// == COMPARAÇÃO IMPLICITA
+
+//const numero = 5;
+//const texto = "5";
+//console.log(numero == texto);
+
+//usar mais o ===
+//fazer as conveções de forma explicita
+//Number();
+//String();
+
+//operador ternario 
+//basicamento um if - encurta o codigo
+
+// const idadeMinima = 18;
+// const idadeCliente = 16;
+
+// if (idadeCliente >= idadeMinima){
+//     console.log('Cachaça');
+// } else {
+//     console.log('Leithe');
+// }
+
+// console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco");
+
+//Templates literais
+
+// const nome = 'ju';
+// const idade = 2021-1981;
+// const cidadeDeNascimento = 'Sao Paulo'
+// //const apresentacao = 'meu nome é' + nome + ', minha idade é ' + idade + ', e eu nasci em: ' + cidadeDeNascimento;
+// const apresentacao = `Meu nome é ${nome}, minha idade é ${idade}, e eu nasci em ${cidadeDeNascimento}.`
+
+// console.log(apresentacao);
+
+
+
+
