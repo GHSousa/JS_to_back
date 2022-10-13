a0150c3ce164d3a1197d7c17f019089343ee5cda
Gabriel Hiago de Sousa <gabrielhiago96@hotmail.com>
2022-10-13 00:08:34 -0300

    Atualização dos arquivos  do curso JS_to_back

---
+++ JS_to_back02.js
@@ -0,0 +1,90 @@
+// var 
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area);
+
+//var area; 
+
+//let 
+
+//let forma = 'retangulo'
+//let altura = 5;
+//let comprimento = 7;
+//let area; 
+//if (forma === 'retangulo') {
+//    area = altura * comprimento
+//} else {
+//    area = (altura * comprimento) / 2; 
+//}
+
+//console.log(altura);
+
+//const
+
+//const forma = 'quadrado'
+//const altura = 5;
+//const comprimento = 7;
+//const area = altura * comprimento;  
+//let area;
+
+//if (forma === 'quadrado' ) {
+//    area = altura * comprimento;
+//} else {
+//    area = (altura * comprimento) / 2;
+//}
+
+//console.log(area)
+
+// boolean
+
+//const usuarioLogado = true 
+//const contaPaga = false
+
+// truthy or falsy 
+
+
+// Linguagem de banco 
+// 0 => false
+// 1 => true
+// (0 == false)
+// ("" == false )
+// (1 == true)
+
+// null => representa vazio ou nada
+// undefined 
+
+//let minhaVar; 
+//let varNull = null;
+
+//let numero = 3;
+//let texto = 'Alura';
+
+
+//(typeof numero) = tipo da variavel e numero;
+//(typeof texto) = tipo da variavel e texto;
+
+//(typeof minhaVar) = tipo da variavel undefined;
+//(typeof varNull) =  tipo da variavel objeto;
+
+//conversoes
+//tipo de dado
+//booleanos
+
+//conversao implicita ==
+//converte um tipo de dado em outro 
+//tipos de igual => = atribuição; == valor independente do tipo; === valor e tipo. 
+
+//const numero = 456;
+//const numeroString = "456"
+// Number() pra transformar string em um numero (N maiusculo)
+// String() pra transformar numero em String (S maiusculo)
+//console.log(numero + Number(numeroString));
+
+
+//conversao explicita
+
+
+
---
+++ JS_to_back03.js
@@ -0,0 +1,55 @@
+// TIPAGEM DINAMICA
+//untyped
+
+
+//let minhaVar = 567;
+//minhaVar = "texto";
+//minhaVar = true;
+
+//multiparadigma
+//varias formas de resolver um programa
+
+// por que se chama JS
+//sao diferentes de java e js. 
+
+//o que é ES6/ES2015
+//versao mais importante
+
+
+//linguagem interpretada
+//codigo executado diretamente
+//compilada tem um compilador transformando pra maquina.
+
+//nodeJS
+//voltado pra back end e não tem o que vai pro front
+
+// ERROS
+
+//SyntaxError: Missing initializer in const declaration 
+//falta alguma informação da declaração da constante
+//inicializador é o valor passado para a constante
+//const nao pode ser mudade nem ficar sem valor
+
+//SyntaxError: missing ) after argument list 
+//falta o ) depois da lista de argumento
+
+//ReferenceError: minhaVar is not defined 
+//a variavel minhaVar não foi informada
+
+//CONSOLE.API
+
+// .log => registro
+
+// tratamento de erro
+
+//console.error('deu erro veio!');
+
+
+
+
+
+
+
+
+
+
